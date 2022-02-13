3f012dd6436e6f6dc062ec452a05231594c178b0
utsavk28 <utsavkhatu416@gmail.com>
2022-02-13 12:27:30 +0530

    Models Configured in Webapp

--- webapp/main.py
+++ webapp/main.py
@@ -3,31 +3,32 @@ import numpy as np
 import sklearn
 from tensorflow import keras
 import streamlit as st
-from model import get_gpt2_simple_novel
+from model import get_gpt2_simple_novel, get_gpt2_novel
 
 
 def main():
-    st.write("""
-             # Hello World!!!
+    model_name = st.sidebar.selectbox('Select Your Novel Generator',
+                                      ("Original GPT2",
+                                       "Japanese Light Novel GPT2", "JK Rowling's GPT2"))
+
+    st.write(f"""
+             # NovelAI
+             ## {model_name}
              """)
 
     text = st.text_input("Story's Beginning (can leave empty)")
     length = st.slider("Length of the Story", 100, 5*10**4)
     run = st.button("Run")
-    story = ""
+
     if run:
-        # story = st.write(get_gpt_novel(text,length))
-        # story = st.write(get_gpt2_simple_novel())
-        txt = get_gpt2_simple_novel()
-        txt = str(txt)
-        print(type(txt),len(txt))
-        print(txt)
-        st.write("Running")
-        st.write(txt)
-        # for t in txt[::100] :
-        #     print(t)
-        #     # st.write(t)
-        #     break
+        story = ""
+        if model_name == "Original GPT2":
+            story = get_gpt2_novel(text, length)
+        elif model_name == "Japanese Light Novel GPT2":
+            story = get_gpt2_simple_novel(text, length, model_no="1")
+        elif model_name == "JK Rowling's GPT2":
+            story = get_gpt2_simple_novel(text, length, model_no=2)
+        st.write(story)
     else:
         st.write("Try Running...")
     # download = st.download_button("Download",story,)
