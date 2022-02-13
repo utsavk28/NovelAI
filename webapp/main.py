b580d7a29794c7d748d7a1c65c68f3cecc46adb6
utsavk28 <utsavkhatu416@gmail.com>
2022-02-13 10:59:31 +0530

    Webapp minor fixes & data added

--- webapp/main.py
+++ webapp/main.py
@@ -3,22 +3,33 @@ import numpy as np
 import sklearn
 from tensorflow import keras
 import streamlit as st
-from model import get_gpt2_novel,get_gpt2_simple_novel
+from model import get_gpt2_simple_novel
+
 
 def main():
     st.write("""
              # Hello World!!!
              """)
-    
+
     text = st.text_input("Story's Beginning (can leave empty)")
-    length = st.slider("Length of the Story",100,5*10**4)
+    length = st.slider("Length of the Story", 100, 5*10**4)
     run = st.button("Run")
     story = ""
-    if run :
+    if run:
         # story = st.write(get_gpt_novel(text,length))
-        story = st.write(get_gpt2_simple_novel())
-    else :
-        st.write("Try Running...") 
+        # story = st.write(get_gpt2_simple_novel())
+        txt = get_gpt2_simple_novel()
+        txt = str(txt)
+        print(type(txt),len(txt))
+        print(txt)
+        st.write("Running")
+        st.write(txt)
+        # for t in txt[::100] :
+        #     print(t)
+        #     # st.write(t)
+        #     break
+    else:
+        st.write("Try Running...")
     # download = st.download_button("Download",story,)
 
 
--- webapp/model.py
+++ webapp/model.py
@@ -1,36 +1,35 @@
 import transformers
 import random
-from transformers import TFGPT2LMHeadModel, GPT2Tokenizer
+# from transformers import TFGPT2LMHeadModel, GPT2Tokenizer
 import tensorflow as tf
 import gpt_2_simple as gpt2
 
 
-tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
-GPT2 = TFGPT2LMHeadModel.from_pretrained(
-    "gpt2", pad_token_id=tokenizer.eos_token_id)
+# tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+# GPT2 = TFGPT2LMHeadModel.from_pretrained(
+#     "gpt2", pad_token_id=tokenizer.eos_token_id)
 
 
-def get_gpt2_novel(text, length):
-    if len(text) == 0:
-        text = random.choice(['A', 'The', 'Once', 'An'])
+# def get_gpt2_novel(text, length):
+#     if len(text) == 0:
+#         text = random.choice(['A', 'The', 'Once', 'An'])
 
-    input_ids = tokenizer.encode(text, return_tensors='tf')
-    sample_outputs = GPT2.generate(
-        input_ids,
-        do_sample=True,
-        max_length=length,
-        #temperature = .8,
-        top_k=50,
-        top_p=0.85
-        #num_return_sequences = 5
-    )
-    return tokenizer.decode(random.choice(sample_outputs), skip_special_tokens=True)
+#     input_ids = tokenizer.encode(text, return_tensors='tf')
+#     sample_outputs = GPT2.generate(
+#         input_ids,
+#         do_sample=True,
+#         max_length=length,
+#         #temperature = .8,
+#         top_k=50,
+#         top_p=0.85
+#         #num_return_sequences = 5
+#     )
+#     return tokenizer.decode(random.choice(sample_outputs), skip_special_tokens=True)
 
 
 def get_gpt2_simple_novel():
-    tf.compat.v1.reset_default_graph()
+    # tf.compat.v1.reset_default_graph()
     sess = gpt2.start_tf_sess()
-    gpt2.load_gpt2(sess,)
+    gpt2.load_gpt2(sess)
     x = gpt2.generate(sess, return_as_list=True,)
     return x[0]
-
